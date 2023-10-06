f2b118576aa2406516b77efed4fd71cd7f182ab1
Manav <30342951+MzaxnaV@users.noreply.github.com>
2023-10-06 14:58:01 +0100

    test appConfig

---
+++ app.config.ts
@@ -0,0 +1,3 @@
+export default defineAppConfig({
+    title: 'My Portfolio Website',
+})
--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const appConfig = useAppConfig()
+</script>
+
 <template>
   <div>
+    {{ appConfig.title }}
     <NuxtWelcome />
   </div>
 </template>
