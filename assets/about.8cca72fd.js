c48bd433be97486e27633ecc8998e2887c7d4281
Manav <30342951+MzaxnaV@users.noreply.github.com>
2023-10-06 15:24:22 +0100

    Updates

---
+++ assets/about.8cca72fd.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.c27b6911.js";import{q as t,m as o,s}from"./entry.ee458a64.js";const a={},c=s("p",null,"This page will be displayed at the /about route.",-1),n=[c];function r(_,l){return o(),t("section",null,n)}const p=e(a,[["render",r]]);export{p as default};
